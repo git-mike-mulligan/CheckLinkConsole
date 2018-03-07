1cfd19a1b48267d9ae9ce7724fad0edc6e51bdec
Michael Mulligan <michael.p.mulligan@gmail.com>
2018-03-07 10:15:46 -0500

    initial commit

---
+++ CheckLinkConsole/LinkChecker.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace CheckLinkConsole
+{
+    public class LinkChecker
+    {
+        public static IEnumerable<string> GetLinks(string page)
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(page);
+            var links = htmlDocument.DocumentNode.SelectNodes("//a[@href]")
+                .Select(n => n.GetAttributeValue("href", string.Empty))
+                .Where(l => !String.IsNullOrEmpty(l))
+                .Where(l => l.StartsWith("http"));
+            return links;
+        }
+    }
+}
